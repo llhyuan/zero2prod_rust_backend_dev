7760305bfd573e1532003a1af146536383ff0ebd
llhyuan <lhyuan.liu21@icloud.com>
2023-12-02 11:14:42 +0100

    play around with actix_web framework

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+
+struct AppState {
+    app_name: String,
+}
+
+#[get("/")]
+async fn hello(data: web::Data<AppState>) -> impl Responder {
+    let app_name = &data.app_name;
+    HttpResponse::Ok().body(format!("Hello world from {app_name}!"))
+}
+
+#[post("/echo")]
+async fn echo(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
+}
+
+async fn manual_hello() -> impl Responder {
+    HttpResponse::Ok().body("Hey there!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .app_data(web::Data::new(AppState {
+                app_name: String::from("Actix"),
+            }))
+            .service(hello)
+            .service(echo)
+            .route("/hey", web::get().to(manual_hello))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
