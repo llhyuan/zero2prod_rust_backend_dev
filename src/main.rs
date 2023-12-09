e4eb3b7468da7de70595c21d367bb75685defb1d
llhyuan <lhyuan.liu21@icloud.com>
2023-12-09 12:41:11 +0100

    Use connection pool to facilitate connecting to database from multiple instances of the backend app.

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,19 @@
 use std::net::TcpListener;
 
+use sqlx::PgPool;
 use zero2prod::{configuration::get_configuration, startup::run};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
     let configurations = get_configuration().expect("Failed to read configuration.");
-    println!("{:#?}", configurations);
+
+    let connection = PgPool::connect(&configurations.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
 
     let addr_to_bind = format!("127.0.0.1:{}", configurations.application_port);
 
     let listener = TcpListener::bind(addr_to_bind).expect("Failed to bind random port.");
 
-    run(listener)?.await
+    run(listener, connection)?.await
 }
