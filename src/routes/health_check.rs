8e381f19ffe4940c3530059ec715e9aa78375d49
llhyuan <lhyuan.liu21@icloud.com>
2023-12-09 12:46:22 +0100

    Refactored code. Isolated each test for database operations, by creating a test database for every test. Clean up to be implemented

--- src/routes/health_check.rs
+++ src/routes/health_check.rs
@@ -1,22 +1,23 @@
 use crate::configuration::get_configuration;
+use crate::configuration::DatabaseSettings;
 use crate::startup::run;
-use sqlx::{Connection, PgConnection};
+use sqlx::{Connection, Executor, PgConnection, PgPool};
 use std::net::TcpListener;
+use uuid::Uuid;
+
+pub struct TestApp {
+    pub address: String,
+    pub connection_pool: PgPool,
+}
 
 #[tokio::test]
 async fn subscribe_returns_a_200_for_valid_form_data() {
-    let app_addr = spawn_app();
-    let configuration = get_configuration().expect("Failed to read configuration");
-    let connection_string = configuration.database.connection_string();
-
-    let _connection = PgConnection::connect(&connection_string)
-        .await
-        .expect("Failed to connect to the Postgres.");
+    let test_app = spawn_app().await;
     let client = reqwest::Client::new();
 
-    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+    let body = "name=le%20guin2q&email=ursulasdf_le_guin%40gmail.com";
     let response = client
-        .post(format!("{}/subscriptions", app_addr))
+        .post(format!("{}/subscriptions", &test_app.address))
         .header("Content-Type", "application/x-www-form-urlencoded")
         .body(body)
         .send()
@@ -26,11 +27,43 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
     assert_eq!(200, response.status().as_u16());
 }
 
-fn spawn_app() -> String {
+pub async fn spawn_app() -> TestApp {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port.");
     let addr = listener.local_addr().unwrap();
-    let server = run(listener).expect("Failed to fireup server for test.");
+
+    let mut configuration = get_configuration().expect("Failed to read configuration");
+
+    configuration.database.database_name = Uuid::new_v4().to_string();
+
+    let connection_pool = configure_database(&configuration.database).await;
+
+    let server = run(listener, connection_pool.clone()).expect("Failed to fireup server for test.");
 
     tokio::spawn(server);
-    format!("http://{addr}")
+    TestApp {
+        address: format!("http://{addr}"),
+        connection_pool,
+    }
+}
+
+async fn configure_database(config: &DatabaseSettings) -> PgPool {
+    let mut connection = PgConnection::connect(&config.connection_string_without_db())
+        .await
+        .expect("Fail to connect to test database.");
+
+    connection
+        .execute(format!(r#"CREATE DATABASE "{}";"#, config.database_name).as_str())
+        .await
+        .expect("Failed to create test database.");
+
+    let connection_pool = PgPool::connect(&config.connection_string())
+        .await
+        .expect("Failed to connect to test database.");
+
+    sqlx::migrate!("./migrations")
+        .run(&connection_pool)
+        .await
+        .expect("Failed to migrate test database.");
+
+    connection_pool
 }
