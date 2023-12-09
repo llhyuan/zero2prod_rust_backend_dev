0d680d7827ee273a5d64d443db71e16fd44ff909
llhyuan <lhyuan.liu21@icloud.com>
2023-12-09 12:44:25 +0100

    Implemented INSERT database API for route /subscriptions

---
+++ src/routes/subscription.rs
@@ -0,0 +1,34 @@
+use actix_web::{web, HttpResponse};
+use chrono::Utc;
+use sqlx::PgPool;
+use uuid::Uuid;
+
+#[derive(serde::Deserialize)]
+pub struct FormDataSubscriber {
+    email: String,
+    name: String,
+}
+
+pub async fn subsribe(
+    form: web::Form<FormDataSubscriber>,
+    connection_pool: web::Data<PgPool>,
+) -> HttpResponse {
+    match sqlx::query!(
+        r#"
+    INSERT INTO subscriptions (id, email, name, subscribed_at)
+    VALUES ($1, $2, $3, $4)"#,
+        Uuid::new_v4(),
+        form.email,
+        form.name,
+        Utc::now(),
+    )
+    .execute(connection_pool.get_ref())
+    .await
+    {
+        Ok(_) => HttpResponse::Ok().finish(),
+        Err(msg) => {
+            println!("Failed to execute query: {}", msg);
+            HttpResponse::InternalServerError().finish()
+        }
+    }
+}
