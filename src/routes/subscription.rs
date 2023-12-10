e29e2852c68f538c4a303eb9137ac57a22c6a161
llhyuan <lhyuan.liu21@icloud.com>
2023-12-10 14:30:20 +0100

    Added logging support

--- src/routes/subscription.rs
+++ src/routes/subscription.rs
@@ -1,5 +1,7 @@
 use actix_web::{web, HttpResponse};
+use chrono::Utc;
 use sqlx::PgPool;
+use uuid::Uuid;
 
 #[derive(serde::Deserialize)]
 pub struct FormDataSubscriber {
@@ -11,6 +13,18 @@ pub async fn subsribe(
     form: web::Form<FormDataSubscriber>,
     connection_pool: web::Data<PgPool>,
 ) -> HttpResponse {
+    let request_id = Uuid::new_v4();
+    log::info!(
+        "Request_id:{} - Adding email:{}, name:{} as a new subscriber.",
+        request_id,
+        form.name,
+        form.email
+    );
+
+    log::info!(
+        "Request_id: {} - Saving new subscriber details in the database",
+        request_id
+    );
     match sqlx::query!(
         r#"
     INSERT INTO subscriptions (id, email, name, subscribed_at)
@@ -23,9 +37,19 @@ pub async fn subsribe(
     .execute(connection_pool.get_ref())
     .await
     {
-        Ok(_) => HttpResponse::Ok().finish(),
-        Err(msg) => {
-            println!("Failed to execute query: {}", msg);
+        Ok(_) => {
+            log::info!(
+                "Request_id: {} - New subscriber details have been saved",
+                request_id
+            );
+            HttpResponse::Ok().finish()
+        }
+        Err(error_msg) => {
+            log::error!(
+                "Request_id: {} - Failed to execute query: {:?}",
+                request_id,
+                error_msg
+            );
             HttpResponse::InternalServerError().finish()
         }
     }
