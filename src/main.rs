9b5278913c74bc4c4c7f9ab472145e9fbfb43579
llhyuan <lhyuan.liu21@icloud.com>
2023-12-14 10:43:47 +0100

    Initialize a global subscriber for tracing

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
-use env_logger::Env;
 use std::net::TcpListener;
 
 use sqlx::PgPool;
-use zero2prod::{configuration::get_configuration, startup::run};
+use zero2prod::{
+    configuration::get_configuration,
+    startup::run,
+    telemetry::{get_subscriber, init_subscriber},
+};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
     // Initialize the logger
-    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+    let subscriber = get_subscriber("zero2prod".into(), "info".into());
+    init_subscriber(subscriber);
 
     let configurations = get_configuration().expect("Failed to read configuration.");
 
