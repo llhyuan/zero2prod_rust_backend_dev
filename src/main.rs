842da86202c2cb60d64dfed56c6aed658ce3444e
llhyuan <lhyuan.liu21@icloud.com>
2023-12-14 10:33:51 +0100

    Added logging middleware

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,19 @@
-use env_logger::Env;
 use std::net::TcpListener;
 
 use sqlx::PgPool;
-use zero2prod::{configuration::get_configuration, startup::run};
+use zero2prod::{
+    configuration::get_configuration,
+    startup::run,
+    telemetry::{get_subscriber, init_subscriber},
+};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    // Initialize the logger
-    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+    // Instrumenting the app
+    let subscriber = get_subscriber("zero2prod".into(), "info".into());
+    init_subscriber(subscriber);
 
+    // Config resources for the app: database connection and TCP listener
     let configurations = get_configuration().expect("Failed to read configuration.");
 
     let connection = PgPool::connect(&configurations.database.connection_string())
@@ -19,5 +24,6 @@ async fn main() -> Result<(), std::io::Error> {
 
     let listener = TcpListener::bind(addr_to_bind).expect("Failed to bind random port.");
 
+    // Startup the server.
     run(listener, connection)?.await
 }
