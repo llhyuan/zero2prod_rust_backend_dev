b7ae89f2222be94cc3409288bb824d2687535561
llhyuan <lhyuan.liu21@icloud.com>
2023-12-15 13:16:53 +0100

    Change production logging target to be stdout. Code addjustment for using Secret crate

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ use zero2prod::{
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
     // Initialize the logger
-    let subscriber = get_subscriber("zero2prod".into(), "info".into(), std::io::stderr);
+    let subscriber = get_subscriber("zero2prod".into(), "info".into(), std::io::stdout);
     init_subscriber(subscriber);
 
     let configurations = get_configuration().expect("Failed to read configuration.");
