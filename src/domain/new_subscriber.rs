3b3356ab001a15aa12cd4137c00a89072c2ae884
llhyuan <lhyuan.liu21@icloud.com>
2023-12-28 14:58:42 +0100

    Implementing TryFrom trait for NewSubscriber

--- src/domain/new_subscriber.rs
+++ src/domain/new_subscriber.rs
@@ -1,7 +1,23 @@
 use crate::domain::subscriber_email::SubscriberEmail;
 use crate::domain::subscriber_name::SubscriberName;
 
+#[derive(serde::Deserialize)]
+pub struct FormDataSubscriber {
+    pub email: String,
+    pub name: String,
+}
+
 pub struct NewSubscriber {
     pub email: SubscriberEmail,
     pub name: SubscriberName,
+}
+
+impl TryFrom<FormDataSubscriber> for NewSubscriber {
+    type Error = String;
+
+    fn try_from(form: FormDataSubscriber) -> Result<Self, Self::Error> {
+        let name = SubscriberName::parse(form.name)?;
+        let email = SubscriberEmail::parse(form.email)?;
+        Ok(Self { name, email })
+    }
 }
