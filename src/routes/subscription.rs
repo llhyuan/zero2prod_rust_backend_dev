e2045e4cba632f366c409c81af988ffbffc00cf6
llhyuan <lhyuan.liu21@icloud.com>
2023-12-28 14:59:43 +0100

    Refactoring to make the type conversion intent clearer

--- src/routes/subscription.rs
+++ src/routes/subscription.rs
@@ -3,13 +3,7 @@ use chrono::Utc;
 use sqlx::PgPool;
 use uuid::Uuid;
 
-use crate::domain::{NewSubscriber, SubscriberEmail, SubscriberName};
-
-#[derive(serde::Deserialize)]
-pub struct FormDataSubscriber {
-    email: String,
-    name: String,
-}
+use crate::domain::{FormDataSubscriber, NewSubscriber, SubscriberEmail, SubscriberName};
 
 #[tracing::instrument(name="Adding a new subscriber", skip(form, connection_pool), fields(subscriber_email=%form.email, subscriber_name=%form.name))]
 // The web::Form<> and web::Data annotations are telling the framework
@@ -20,16 +14,11 @@ pub async fn subsribe(
     connection_pool: web::Data<PgPool>,
 ) -> HttpResponse {
     let form: FormDataSubscriber = form.into_inner();
-    let name = match SubscriberName::parse(form.name) {
-        Ok(name) => name,
-        Err(_) => return HttpResponse::BadRequest().finish(),
-    };
-    let email = match SubscriberEmail::parse(form.email) {
-        Ok(email) => email,
-        Err(_) => return HttpResponse::BadRequest().finish(),
-    };
 
-    let new_subscriber = NewSubscriber { email, name };
+    let new_subscriber = match NewSubscriber::try_from(form) {
+        Ok(subscriber) => subscriber,
+        Err(_) => return HttpResponse::BadRequest().finish(),
+    };
     match insert_subscriber(&new_subscriber, &connection_pool).await {
         Ok(_) => HttpResponse::Ok().finish(),
         Err(_) => HttpResponse::InternalServerError().finish(),
@@ -61,4 +50,10 @@ async fn insert_subscriber(
         err
     })?;
     Ok(())
+}
+
+pub fn parse_subscriber(form: FormDataSubscriber) -> Result<NewSubscriber, String> {
+    let name = SubscriberName::parse(form.name)?;
+    let email = SubscriberEmail::parse(form.email)?;
+    Ok(NewSubscriber { name, email })
 }
