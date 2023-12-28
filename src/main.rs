df53cd58c0348660ed6579641aac65e81903c49c
llhyuan <lhyuan.liu21@icloud.com>
2023-12-28 15:00:23 +0100

    Added comment

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,7 @@ use zero2prod::{
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
     // Initialize the logger
+    // On the application level
     let subscriber = get_subscriber("zero2prod".into(), "info".into(), std::io::stdout);
     init_subscriber(subscriber);
 
