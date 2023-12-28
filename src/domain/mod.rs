66555837c25eea34e42fcd6da8dad19f44114fe8
llhyuan <lhyuan.liu21@icloud.com>
2023-12-28 15:03:13 +0100

    Moved FormDataSubsriber and NewSubscriber definition to Domain module

--- src/domain/mod.rs
+++ src/domain/mod.rs
@@ -3,6 +3,7 @@ mod subscriber_email;
 mod subscriber_name;
 
 // expose chosen features on a sub-crate level
+pub use new_subscriber::FormDataSubscriber;
 pub use new_subscriber::NewSubscriber;
 pub use subscriber_email::SubscriberEmail;
 pub use subscriber_name::SubscriberName;
