5c21bc02760f3cb90694f96695b8fc9b0963f29d
llhyuan <lhyuan.liu21@icloud.com>
2024-01-18 07:35:28 +0100

    code refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,6 @@
-use std::net::TcpListener;
-
-use sqlx::postgres::PgPoolOptions;
 use zero2prod::{
     configuration::get_configuration,
-    email_clients::EmailClient,
-    startup::run,
+    startup::Application,
     telemetry::{get_subscriber, init_subscriber},
 };
 
@@ -17,28 +13,8 @@ async fn main() -> Result<(), std::io::Error> {
 
     let configurations = get_configuration().expect("Failed to read configuration.");
 
-    let connection = PgPoolOptions::new().connect_lazy_with(configurations.database.with_db());
+    let application = Application::build(configurations).await?;
 
-    let addr_to_bind = format!(
-        "{}:{}",
-        configurations.application.host, configurations.application.port
-    );
-
-    let listener = TcpListener::bind(addr_to_bind).expect("Failed to bind random port.");
-
-    let sender_email = configurations
-        .email_client
-        .sender()
-        .expect("Invalide sender email.");
-
-    let timeout = configurations.email_client.timeout();
-
-    let email_client = EmailClient::new(
-        configurations.email_client.base_url,
-        sender_email,
-        configurations.email_client.auth_token,
-        timeout,
-    );
-
-    run(listener, connection, email_client)?.await
+    application.run_until_stopped().await?;
+    Ok(())
 }
