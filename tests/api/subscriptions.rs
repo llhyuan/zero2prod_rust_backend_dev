2419ebf93996eae4d7a5e4b1328e195b24762722
llhyuan <lhyuan.liu21@icloud.com>
2024-01-19 14:17:58 +0100

    code refactored

--- tests/api/subscriptions.rs
+++ tests/api/subscriptions.rs
@@ -1,4 +1,3 @@
-use linkify::LinkFinder;
 use wiremock::{
     matchers::{any, method},
     Mock, ResponseTemplate,
@@ -44,14 +43,30 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
     let response = test_app.post_subscription(body).await;
 
     assert_eq!(200, response.status().as_u16());
+}
 
-    let saved = sqlx::query!("SELECT email, name FROM subscriptions",)
+#[tokio::test]
+async fn subcribe_persists_the_new_subcriber() {
+    let test_app = spawn_app().await;
+
+    let body = String::from("name=le%20guin&email=ursula_le_guin%40gmail.com");
+
+    Mock::given(any())
+        .and(method("POST"))
+        .respond_with(ResponseTemplate::new(200))
+        .mount(&test_app.email_server)
+        .await;
+
+    test_app.post_subscription(body).await;
+
+    let saved = sqlx::query!("SELECT email, name, status FROM subscriptions",)
         .fetch_one(&test_app.connection_pool)
         .await
-        .expect("Failed to fetch saved subscription.");
+        .expect("Failed to fetch saved subscriber.");
 
     assert_eq!(saved.email, "ursula_le_guin@gmail.com");
     assert_eq!(saved.name, "le guin");
+    assert_eq!(saved.status, "pending_confirmation");
 }
 
 #[tokio::test]
@@ -93,7 +108,7 @@ async fn subscribe_sends_a_confirmation_email_for_valid_data() {
 }
 
 #[tokio::test]
-async fn subscribe_sneds_a_confirmation_email_with_a_link() {
+async fn subscribe_sends_a_confirmation_email_with_a_link() {
     let test_app = spawn_app().await;
 
     let body = String::from("name=le%20guin&email=ursula_le_guin%40gmail.com");
@@ -106,17 +121,7 @@ async fn subscribe_sneds_a_confirmation_email_with_a_link() {
     let _ = test_app.post_subscription(body).await;
 
     let email_request = &test_app.email_server.received_requests().await.unwrap()[0];
-    let request_body: serde_json::Value = serde_json::from_slice(&email_request.body).unwrap();
+    let confirmation_links = test_app.get_confirmation_link(email_request);
 
-    fn get_link(s: &str) -> String {
-        let links: Vec<_> = LinkFinder::new().links(s).collect();
-        println!("{:?}", links);
-        links[0].as_str().to_string()
-    }
-    println!("{:?}", request_body);
-
-    let html_link = get_link(request_body["HtmlBody"].as_str().unwrap());
-    let text_link = get_link(request_body["TextBody"].as_str().unwrap());
-
-    assert_eq!(html_link, text_link);
+    assert_eq!(confirmation_links.html, confirmation_links.plain_text);
 }
