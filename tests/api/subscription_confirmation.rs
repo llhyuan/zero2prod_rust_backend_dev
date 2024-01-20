03b8829b19d4893cb3ff435621ecf4bf1c9d35d2
llhyuan <lhyuan.liu21@icloud.com>
2024-01-20 09:05:25 +0100

    added intergration tests for subcription confirmation API

--- tests/api/subscription_confirmation.rs
+++ tests/api/subscription_confirmation.rs
@@ -40,3 +40,35 @@ async fn the_link_returned_by_subscribe_returns_a_200_when_called() {
 
     assert_eq!(200, response.status().as_u16());
 }
+
+#[tokio::test]
+async fn click_on_the_confirmation_link_confirms_a_subscriber() {
+    let test_app = spawn_app().await;
+
+    let body = String::from("name=le%20guin&email=ursula_le_guin%40gmail.com");
+
+    Mock::given(any())
+        .and(method("POST"))
+        .respond_with(ResponseTemplate::new(200))
+        .mount(&test_app.email_server)
+        .await;
+
+    test_app.post_subscription(body).await;
+
+    let email_request = &test_app.email_server.received_requests().await.unwrap()[0];
+    let confirmation_links = test_app.get_confirmation_link(email_request);
+    let parsed_link = confirmation_links.html;
+
+    reqwest::get(parsed_link)
+        .await
+        .expect("Faield to follow the confirmation link.");
+
+    let saved = sqlx::query!("SELECT email, name, status FROM subscriptions",)
+        .fetch_one(&test_app.connection_pool)
+        .await
+        .expect("Failed to fetch saved subscription.");
+
+    assert_eq!(saved.email, "ursula_le_guin@gmail.com");
+    assert_eq!(saved.name, "le guin");
+    assert_eq!(saved.status, "confirmed");
+}
