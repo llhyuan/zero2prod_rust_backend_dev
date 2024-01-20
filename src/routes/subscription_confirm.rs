abc95749a388ab27d468bb238767da418cff7c34
llhyuan <lhyuan.liu21@icloud.com>
2024-01-20 09:03:12 +0100

    completed the subcription confirmation API

--- src/routes/subscription_confirm.rs
+++ src/routes/subscription_confirm.rs
@@ -1,4 +1,6 @@
 use actix_web::{web, HttpResponse};
+use sqlx::PgPool;
+use uuid::Uuid;
 
 #[derive(serde::Deserialize)]
 pub struct Parameters {
@@ -6,8 +8,59 @@ pub struct Parameters {
 }
 
 #[tracing::instrument(name = "Confirming a pending subscription", skip_all)]
-pub async fn subscription_confirm(parameters: web::Query<Parameters>) -> HttpResponse {
-    println!("{}", parameters.subscription_token);
-    HttpResponse::Ok().finish()
+pub async fn subscription_confirm(
+    parameters: web::Query<Parameters>,
+    connection_pool: web::Data<PgPool>,
+) -> HttpResponse {
+    let id = match get_subscriber_id(&connection_pool, &parameters.subscription_token).await {
+        Ok(id) => id,
+        Err(_) => return HttpResponse::InternalServerError().finish(),
+    };
+
+    match id {
+        None => HttpResponse::Unauthorized().finish(),
+        Some(subscriber_id) => {
+            if update_status(&connection_pool, subscriber_id)
+                .await
+                .is_err()
+            {
+                HttpResponse::InternalServerError().finish()
+            } else {
+                HttpResponse::Ok().finish()
+            }
+        }
+    }
 }
 
+async fn get_subscriber_id(
+    connection_pool: &PgPool,
+    subscription_token: &str,
+) -> Result<Option<Uuid>, sqlx::Error> {
+    let result = sqlx::query!(
+        "SELECT subscriber_id FROM subscription_tokens WHERE subscription_token = $1",
+        subscription_token,
+    )
+    .fetch_optional(connection_pool)
+    .await
+    .map_err(|err| {
+        tracing::error!("Failed to execute query: {:?}", err);
+        err
+    })?;
+
+    Ok(result.map(|r| r.subscriber_id))
+}
+
+async fn update_status(connection_pool: &PgPool, subscriber_id: Uuid) -> Result<(), sqlx::Error> {
+    sqlx::query!(
+        "UPDATE subscriptions SET status = 'confirmed' WHERE id = $1",
+        subscriber_id
+    )
+    .execute(connection_pool)
+    .await
+    .map_err(|e| {
+        tracing::error!("Failed to execute query: {:?}", e);
+        e
+    })?;
+
+    Ok(())
+}
